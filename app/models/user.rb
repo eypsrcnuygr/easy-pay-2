19af79fe9ecd43c1ab33257a5bade80c7acffbe3
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-06 17:42:13 +0300

    Add sign in with gitHub functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   has_many :groups, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
   has_secure_password
+
+  def name=(value)
+    super(value.try(:strip))
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, id: auth.uid, password_digest: '12345678')
+      .or(where(name: auth['info']['nickname'][0..19])).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.id = auth['uid']
+      user.name = auth['info']['nickname'][0..19]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get '/assigned_transactions', to: 'transactions#assigned_transactions', as: 'assigned'
   get '/unassigned_transactions', to: 'transactions#unassigned_transactions', as: 'unassigned'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'auth/:provider/callback', to: 'sessions#create' 
 end
