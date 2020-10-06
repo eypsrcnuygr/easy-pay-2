55e4d26aa6ed0c7465e1898022339b8bed645854
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-06 17:54:16 +0300

    Add provider to user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,6 +77,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :password)
+      params.require(:user).permit(:name, :password, provider: nil)
     end
 end
