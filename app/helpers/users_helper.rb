2e0f9a6044a2f725f31d3f47ec3083abb2a1d294
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-08 16:24:48 +0300

    Take all the logic from users view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,33 @@
 module UsersHelper
-  def user_logged_in?
+  def buttons_for_user
     return unless logged_in?
 
     render 'buttons_for_logged_in_user'
+  end
+
+  def internal_transactions_exist?
+    if @internal_transactions.length == 0
+      render 'non_exist_internal_transactions'
+    else
+      render 'exist_internal_transactions'
+    end
+  end
+
+  def external_transactions_exist?
+    if @external_transactions.length == 0
+      render 'non_exist_external_transactions'
+    else
+      render 'exist_external_transactions'
+    end
+  end
+
+  def groups_exist?
+    if @groups.length == 0
+      render 'non_exist_groups'
+    else
+      render 'exist_groups'
+    end
+
+   
   end
 end
