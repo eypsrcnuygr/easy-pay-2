08dae90647175533ee01a0fa7b1e56e3db8a50df
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-08 18:27:58 +0300

    Move groups logic to helper

---
+++ app/helpers/groups_helper.rb
@@ -0,0 +1,41 @@
+module GroupsHelper
+  def logged_in_index?
+    if logged_in?
+      render 'logged_in_index'
+    else
+      render 'not_authorized'
+    end
+  end
+
+  def current_user_groups?
+    if @group.user == current_user
+      render 'current_user_buttons'
+    else
+      render 'back_buttons'
+    end
+  end
+
+  def groups_show?
+    if logged_in?
+      render 'logged_in_show'
+    else
+      render 'not_authorized'
+    end
+  end
+
+  def logged_in_new?
+    if logged_in?
+      render 'logged_in_new'
+    else
+      render 'not_authorized'
+    end
+  end
+
+  def logged_in_edit?
+    if logged_in?
+      render 'logged_in_edit'
+    else
+      render 'not_authorized'
+    end
+  end
+end
