7a91db60f057e684fb44e6bf3acb1ce045b92a89
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-09 15:01:01 +0300

    Add negative test cases for loging in

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -33,4 +33,46 @@ RSpec.feature "Users", type: :feature do
     expect(page).to have_content('Logged in succesfuly')
     expect(page).to have_content('Logout')
   end
+  it 'prohibits user to to log in with incorrext password' do
+    click_link 'Logout'
+    click_link 'SignIn'
+    within('form') do
+      fill_in 'Name', with: 'user1'
+      fill_in 'Password', with: 1357924689
+    end
+    click_button 'Sign In'
+    expect(page).to have_content('There was something wrong with your login information')
+    expect { visit(user_path(5)) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+  it 'prohibits user to to log in with incorrext name' do
+    click_link 'Logout'
+    click_link 'SignIn'
+    within('form') do
+      fill_in 'Name', with: 'user'
+      fill_in 'Password', with: 135792468
+    end
+    click_button 'Sign In'
+    expect(page).to have_content('There was something wrong with your login information')
+    expect { visit(user_path(6)) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+  it 'prevent not-logged in user to navigate through new transaction' do
+    click_link 'Logout'
+    visit(new_transaction_path)
+    expect(page).to have_content('You are not authorized to continue')
+  end
+  it 'prevent not-logged in user to navigate through groups' do
+    click_link 'Logout'
+    visit(groups_path)
+    expect(page).to have_content('You are not authorized to see this page')
+  end
+  it 'prevent not-logged in user to navigate through users' do
+    click_link 'Logout'
+    visit(users_path)
+    expect(page).to have_content('You are not authorized to see this page')
+  end
+  it 'prevent not-logged in user to navigate through transactions' do
+    click_link 'Logout'
+    visit(transactions_path)
+    expect(page).to have_content('You are not authorized to continue')
+  end
 end
