e198dc9ace174da9e4e7cabdcefb3883c8407017
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-10-09 10:46:34 +0300

    Add validations for transactions and groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :group_transactions, dependent: :destroy
   has_many :transactions, through: :group_transactions, source: :owner
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
 end
